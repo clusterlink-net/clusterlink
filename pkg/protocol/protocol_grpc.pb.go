b97018a610c88d0be63f21beb5b49254762a0879
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2022-11-23 16:26:37 +0530

    Add Disconnect service

--- pkg/protocol/protocol_grpc.pb.go
+++ pkg/protocol/protocol_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.12.4
-// source: pkg/protocol/protocol.proto
+// source: protocol.proto
 
 package protocol
 
@@ -103,7 +103,7 @@ var Expose_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "pkg/protocol/protocol.proto",
+	Metadata: "protocol.proto",
 }
 
 // ConnectClient is the client API for Connect service.
@@ -191,7 +191,95 @@ var Connect_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "pkg/protocol/protocol.proto",
+	Metadata: "protocol.proto",
+}
+
+// DisconnectClient is the client API for Disconnect service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type DisconnectClient interface {
+	// Sends a connect message
+	DisconnectCmd(ctx context.Context, in *DisconnectRequest, opts ...grpc.CallOption) (*DisconnectReply, error)
+}
+
+type disconnectClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewDisconnectClient(cc grpc.ClientConnInterface) DisconnectClient {
+	return &disconnectClient{cc}
+}
+
+func (c *disconnectClient) DisconnectCmd(ctx context.Context, in *DisconnectRequest, opts ...grpc.CallOption) (*DisconnectReply, error) {
+	out := new(DisconnectReply)
+	err := c.cc.Invoke(ctx, "/protocol.Disconnect/DisconnectCmd", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// DisconnectServer is the server API for Disconnect service.
+// All implementations must embed UnimplementedDisconnectServer
+// for forward compatibility
+type DisconnectServer interface {
+	// Sends a connect message
+	DisconnectCmd(context.Context, *DisconnectRequest) (*DisconnectReply, error)
+	mustEmbedUnimplementedDisconnectServer()
+}
+
+// UnimplementedDisconnectServer must be embedded to have forward compatible implementations.
+type UnimplementedDisconnectServer struct {
+}
+
+func (UnimplementedDisconnectServer) DisconnectCmd(context.Context, *DisconnectRequest) (*DisconnectReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DisconnectCmd not implemented")
+}
+func (UnimplementedDisconnectServer) mustEmbedUnimplementedDisconnectServer() {}
+
+// UnsafeDisconnectServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to DisconnectServer will
+// result in compilation errors.
+type UnsafeDisconnectServer interface {
+	mustEmbedUnimplementedDisconnectServer()
+}
+
+func RegisterDisconnectServer(s grpc.ServiceRegistrar, srv DisconnectServer) {
+	s.RegisterService(&Disconnect_ServiceDesc, srv)
+}
+
+func _Disconnect_DisconnectCmd_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DisconnectRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DisconnectServer).DisconnectCmd(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/protocol.Disconnect/DisconnectCmd",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DisconnectServer).DisconnectCmd(ctx, req.(*DisconnectRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// Disconnect_ServiceDesc is the grpc.ServiceDesc for Disconnect service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Disconnect_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "protocol.Disconnect",
+	HandlerType: (*DisconnectServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "DisconnectCmd",
+			Handler:    _Disconnect_DisconnectCmd_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "protocol.proto",
 }
 
 // HelloClient is the client API for Hello service.
@@ -279,5 +367,5 @@ var Hello_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "pkg/protocol/protocol.proto",
+	Metadata: "protocol.proto",
 }
