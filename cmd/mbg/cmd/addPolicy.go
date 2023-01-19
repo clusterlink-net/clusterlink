d2d2475bd9800666c6b5a0780c1e9edd2653db1e
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2023-01-19 10:54:52 +0530

    Support adding policy dispatcher

--- cmd/mbg/cmd/addPolicy.go
+++ cmd/mbg/cmd/addPolicy.go
@@ -5,10 +5,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"strconv"
-
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.ibm.com/mbg-agent/cmd/mbg/state"
 	"github.ibm.com/mbg-agent/pkg/policyEngine"
 )
 
@@ -18,25 +16,22 @@ var addPolicyCmd = &cobra.Command{
 	Short: "add the list of Policies that the MBG supports",
 	Long:  `add the list of Policies that the MBG supports`,
 	Run: func(cmd *cobra.Command, args []string) {
-		name, _ := cmd.Flags().GetString("name")
-		desc, _ := cmd.Flags().GetString("desc")
 		target, _ := cmd.Flags().GetString("target")
-		ptype, _ := cmd.Flags().GetString("type")
+		start, _ := cmd.Flags().GetBool("start")
 
-		policyType, err := strconv.Atoi(ptype)
-		if err == nil {
-			policyEngine.AddPolicy(name, desc, target, policyType)
-		} else {
-			log.Errorf("Invalid Policy Type")
+		state.UpdateState()
+		state.GetEventManager().AssignPolicyDispatcher("http://" + target + "/policy")
+		state.SaveState()
+		if start {
+			policyEngine.StartPolicyDispatcher(state.GetChiRouter(), target)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addPolicyCmd)
-
 	addPolicyCmd.Flags().String("name", "", "Policy Name")
 	addPolicyCmd.Flags().String("desc", "", "Short description of policy")
 	addPolicyCmd.Flags().String("target", "", "Target endpoint(e.g.ip:port) to reach the policy agent")
-	addPolicyCmd.Flags().String("type", "", "Policy Type")
+	addPolicyCmd.Flags().Bool("start", true, "Start the policy dispatcher (true/false)")
 }
