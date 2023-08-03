4ee743b193d71406dcdf01213c08b0cc7fd8b577
Or Ozeri <oro@il.ibm.com>
2023-08-03 18:57:16 +0300

    controplane: Introduce gRPC server
    This commit adds the controlplane gRPC server which implements
    the ADS (aggregated discovery service) xDS protocol for envoy clusters and listeners.
    
    Signed-off-by: Or Ozeri <oro@il.ibm.com>

---
+++ pkg/controlplane/server/grpc/server.go
@@ -0,0 +1,46 @@
+package grpc
+
+import (
+	"context"
+	"crypto/tls"
+
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/cache/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/resource/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/server/v3"
+
+	"github.ibm.com/mbg-agent/pkg/controlplane"
+	"github.ibm.com/mbg-agent/pkg/util/grpc"
+)
+
+// Server implements an xDS server for dataplane dynamic configuration.
+type Server struct {
+	grpc.Server
+}
+
+// NewServer returns a new xDS server.
+func NewServer(cp *controlplane.Instance, tlsConfig *tls.Config) *Server {
+	// create a combined mux cache of listeners, clusters and secrets
+	muxCache := &cache.MuxCache{
+		Classify: func(req *cache.Request) string {
+			return req.TypeUrl
+		},
+		ClassifyDelta: func(req *cache.DeltaRequest) string {
+			return req.TypeUrl
+		},
+		Caches: map[string]cache.Cache{
+			resource.ClusterType:  cp.GetXDSClusterManager(),
+			resource.ListenerType: cp.GetXDSListenerManager(),
+		},
+	}
+
+	srv := server.NewServer(context.Background(), muxCache, nil)
+	s := &Server{
+		Server: grpc.NewServer("controlplane-grpc", tlsConfig),
+	}
+
+	grpcServer := s.GetGRPCServer()
+	discovery.RegisterAggregatedDiscoveryServiceServer(grpcServer, srv)
+
+	return s
+}
