d2f911c2245c68e50c6d907f36ef23edac5c05ff
Kfir Toledo <kfir.toledo@ibm.com>
2023-10-01 14:25:15 +0300

    deployment/k8s: Move to use Client.client
    Signed-off-by: Kfir Toledo <kfir.toledo@ibm.com>

--- pkg/deployment/k8s/deployment.go
+++ pkg/deployment/k8s/deployment.go
@@ -2,12 +2,14 @@ package k8s
 
 import (
 	"context"
+	"fmt"
 
-	v1 "k8s.io/api/core/v1"
+	"github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 const (
@@ -16,52 +18,78 @@ const (
 
 // Deployment represents a k8s deployment.
 type Deployment struct {
-	clientset *kubernetes.Clientset
+	client    client.Client
 	namespace string
+	logger    *logrus.Entry
 }
 
 // CreateService creates a service.
 func (d *Deployment) CreateService(name string, port, targetPort uint16) error {
-	serviceSpec := &v1.Service{
-		ObjectMeta: metav1.ObjectMeta{Name: name},
-		Spec: v1.ServiceSpec{
-			Ports: []v1.ServicePort{
+	serviceSpec := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: d.namespace},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
 				{
-					Protocol:   v1.ProtocolTCP,
+					Protocol:   corev1.ProtocolTCP,
 					Port:       int32(port),
 					TargetPort: intstr.FromInt(int(targetPort)),
 				},
 			},
-			Type:     v1.ServiceTypeClusterIP,
+			Type:     corev1.ServiceTypeClusterIP,
 			Selector: map[string]string{"app": dataplaneAppName},
 		},
 	}
 
-	_, err := d.clientset.CoreV1().Services(d.namespace).Create(
-		context.TODO(), serviceSpec, metav1.CreateOptions{})
-	return err
+	err := d.client.Create(context.Background(), serviceSpec)
+	if err != nil {
+		d.logger.Errorf("error occurred while creating K8s service %v:", err)
+		return err
+	}
+
+	d.logger.Infof("Creating K8s service at %s:%d.", name, port)
+	return nil
+}
+
+// DeleteService deletes a service.
+func (d *Deployment) DeleteService(name string) error {
+	serviceSpec := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: d.namespace}}
+
+	err := d.client.Delete(context.Background(), serviceSpec)
+	if err != nil {
+		d.logger.Errorf("error occurred while deleting K8s service %v:", err)
+		return err
+	}
+
+	d.logger.Infof("Deleting K8s service %s.", name)
+	return nil
 }
 
 // UpdateService updates a service.
 func (d *Deployment) UpdateService(name string, port, targetPort uint16) error {
-	serviceSpec := &v1.Service{
-		ObjectMeta: metav1.ObjectMeta{Name: name},
-		Spec: v1.ServiceSpec{
-			Ports: []v1.ServicePort{
+	serviceSpec := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: d.namespace},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
 				{
-					Protocol:   v1.ProtocolTCP,
+					Protocol:   corev1.ProtocolTCP,
 					Port:       int32(port),
 					TargetPort: intstr.FromInt(int(targetPort)),
 				},
 			},
-			Type:     v1.ServiceTypeClusterIP,
+			Type:     corev1.ServiceTypeClusterIP,
 			Selector: map[string]string{"app": dataplaneAppName},
 		},
 	}
 
-	_, err := d.clientset.CoreV1().Services(d.namespace).Update(
-		context.TODO(), serviceSpec, metav1.UpdateOptions{})
-	return err
+	err := d.client.Update(context.Background(), serviceSpec)
+	if err != nil {
+		d.logger.Errorf("error occurred while updating K8s service %v:", err)
+		return err
+	}
+
+	d.logger.Infof("Updating K8s service at %s:%d.", name, port)
+	return nil
 }
 
 // DeleteService deletes a service.
@@ -72,19 +100,33 @@ func (d *Deployment) DeleteService(name string) error {
 
 // NewDeployment returns a new Kubernetes deployment.
 func NewDeployment() (*Deployment, error) {
-	config, err := rest.InClusterConfig()
+	logger := logrus.WithField("component", "k8s-deployment")
+	cl, err := client.New(config.GetConfigOrDie(), client.Options{})
 	if err != nil {
-		return nil, err
+		return &Deployment{}, err
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	// Get namespace
+	var podList corev1.PodList
+	labelSelector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
+		MatchLabels: map[string]string{"app": "cl-controlplane"}})
 	if err != nil {
-		return nil, err
+		return &Deployment{}, err
 	}
 
-	// TODO: support non-default namespace
+	listOptions := &client.ListOptions{LabelSelector: labelSelector}
+	err = cl.List(context.Background(), &podList, listOptions)
+	if err != nil {
+		return &Deployment{}, err
+	}
+
+	if len(podList.Items) == 0 {
+		return &Deployment{}, fmt.Errorf("pod not found")
+	}
+	clNameSpace := podList.Items[0].Namespace
 	return &Deployment{
-		clientset: clientset,
-		namespace: v1.NamespaceDefault,
+		client:    cl,
+		namespace: clNameSpace,
+		logger:    logger,
 	}, nil
 }
