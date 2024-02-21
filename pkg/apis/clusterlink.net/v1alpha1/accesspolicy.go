26fc227d445751423fce500ee76736c865fe999b
Ziv Nevo <nevo@il.ibm.com>
2024-02-21 04:34:10 -0600

    Adding comments to various fields
    Signed-off-by: Ziv Nevo <nevo@il.ibm.com>

--- pkg/apis/clusterlink.net/v1alpha1/accesspolicy.go
+++ pkg/apis/clusterlink.net/v1alpha1/accesspolicy.go
@@ -46,16 +46,22 @@ type WorkloadSetOrSelectorList []WorkloadSetOrSelector
 // WorkloadSetOrSelector describes a set of workloads, based on their attributes (labels)
 // Exactly one of the two fields should be non-empty.
 type WorkloadSetOrSelector struct {
-	WorkloadSets     []string              `json:"workloadSets,omitempty"`
+	// WorkloadSets allows specifying predefined sets of workloads - not yet supported
+	WorkloadSets []string `json:"workloadSets,omitempty"`
+	// WorkloadSelector is a K8s-style label selector, selecting Pods and Services according to their labels
 	WorkloadSelector *metav1.LabelSelector `json:"workloadSelector,omitempty"`
 }
 
 // AccessPolicySpec contains all attributes of an access policy.
 type AccessPolicySpec struct {
-	Privileged bool                      `json:"privileged"`
-	Action     AccessPolicyAction        `json:"action"`
-	From       WorkloadSetOrSelectorList `json:"from"`
-	To         WorkloadSetOrSelectorList `json:"to"`
+	// Privileged is true if the policy has higher priority over non-privileged policies
+	Privileged bool `json:"privileged"`
+	// Action specifies whether the policy allows or denies connections matching its From and To fields
+	Action AccessPolicyAction `json:"action"`
+	// From specifies the set of source workload to which this policy refers
+	From WorkloadSetOrSelectorList `json:"from"`
+	// To specifies the set of destination services to which this policy refers
+	To WorkloadSetOrSelectorList `json:"to"`
 }
 
 // +kubebuilder:object:root=true
