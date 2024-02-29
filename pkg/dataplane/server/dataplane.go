ca9315973e822a9b939fb01e729c08b62c346994
Pravein-Govindan-Kannan <Pravein.Govindan.Kannan@ibm.com>
2024-02-28 16:10:54 -0800

    Add update/delete operation to clusters and listeners in go dataplane
    Signed-off-by: Pravein-Govindan-Kannan <Pravein.Govindan.Kannan@ibm.com>

--- pkg/dataplane/server/dataplane.go
+++ pkg/dataplane/server/dataplane.go
@@ -61,16 +61,31 @@ func (d *Dataplane) GetClusterHost(name string) (string, error) {
 		d.clusters[name].LoadAssignment.GetEndpoints()[0].LbEndpoints[0].GetEndpoint().Hostname, ":")[0], nil
 }
 
-// AddCluster adds a cluster to the map.
+// AddCluster adds/updates a cluster to the map.
 func (d *Dataplane) AddCluster(c *cluster.Cluster) {
 	d.clusters[c.Name] = c
+}
+
+// RemoveCluster adds a cluster to the map.
+func (d *Dataplane) RemoveCluster(name string) {
+	delete(d.clusters, name)
+}
+
+// GetClusters returns the listeners map
+func (d *Dataplane) GetClusters() map[string]*cluster.Cluster {
+	return d.clusters
 }
 
 // AddListener adds a listener to the map.
 func (d *Dataplane) AddListener(ln *listener.Listener) {
 	listenerName := strings.TrimPrefix(ln.Name, api.ImportListenerPrefix)
-	if _, ok := d.listeners[listenerName]; ok {
-		return
+	if listener, ok := d.listeners[listenerName]; ok {
+		// Check if there is an update to the listener address/port
+		if ln.Address.GetSocketAddress().GetAddress() == listener.Address.GetSocketAddress().GetAddress() &&
+			ln.Address.GetSocketAddress().GetPortValue() == listener.Address.GetSocketAddress().GetPortValue() {
+			return
+		}
+		d.DeleteListener(listenerName)
 	}
 	d.listeners[listenerName] = ln
 	go func() {
@@ -78,6 +93,17 @@ func (d *Dataplane) AddListener(ln *listener.Listener) {
 			ln.Address.GetSocketAddress().GetAddress(),
 			ln.Address.GetSocketAddress().GetPortValue())
 	}()
+}
+
+// RemoveListener removes a listener
+func (d *Dataplane) RemoveListener(name string) {
+	delete(d.listeners, name)
+	d.DeleteListener(name)
+}
+
+// GetListeners returns the listeners map
+func (d *Dataplane) GetListeners() map[string]*listener.Listener {
+	return d.listeners
 }
 
 // NewDataplane returns a new dataplane HTTP server.
